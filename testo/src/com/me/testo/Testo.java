409e3d99207fd2c028230c4defbf95766c849f11
Carlos <carlosburon@gmail.com>
2012-09-16 09:48:46 +0200

    Faltaba cargar la camara y el spritebatch, pero eliminando toda referencia a la carga del logo de libgdx. Modificado tambien el color de fondo (ahora es verde). ---- Carga un fondo verde y el  mp3 de lluvia.

--- testo/src/com/me/testo/Testo.java
+++ testo/src/com/me/testo/Testo.java
@@ -36,6 +36,20 @@ public class Testo implements ApplicationListener {
 	      // start the playback of the background music immediately
 	      rainMusic.setLooping(true);
 	      rainMusic.play();
+			
+			camera = new OrthographicCamera();
+			camera.setToOrtho(false, 800, 480);
+			batch = new SpriteBatch();
+			
+//			texture = new Texture(Gdx.files.internal("data/libgdx.png")); todo esto sobra, es parte del logo
+//			texture.setFilter(TextureFilter.Linear, TextureFilter.Linear);
+//			
+//			TextureRegion region = new TextureRegion(texture, 0, 0, 512, 275);
+//			
+//			sprite = new Sprite(region);
+//			sprite.setSize(0.9f, 0.9f * sprite.getHeight() / sprite.getWidth());
+//			sprite.setOrigin(sprite.getWidth()/2, sprite.getHeight()/2);
+//			sprite.setPosition(-sprite.getWidth()/2, -sprite.getHeight()/2);
 	}
 
 	@Override
@@ -46,12 +60,12 @@ public class Testo implements ApplicationListener {
 
 	@Override
 	public void render() {		
-		Gdx.gl.glClearColor(1, 1, 1, 1);
+		Gdx.gl.glClearColor(0, 1, 0, 1); // modificado a color verde, estaba en blanco (1, 1, 1) el ultimo valor es el canal alfa
 		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
 		
 		batch.setProjectionMatrix(camera.combined);
 		batch.begin();
-		sprite.draw(batch);
+		//sprite.draw(batch);
 		batch.end();
 	}
 
